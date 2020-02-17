2600e1afda25afd11c337ac461c3c630abbc7cf8
Michael Brown <mbrown@fensystems.co.uk>
2020-02-17 22:39:16 +0000

    test: Add helper methods to locate package resources
    Add helper methods to retrieve resources that may be located in any
    package along the class hierarchy.
    
    This allows shared test files (such as LDIF files) to be distributed
    as part of the idiosync.test package, while non-shared test files may
    be placed alongside the test case package itself.
    
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

--- idiosync/test/common.py
+++ idiosync/test/common.py
@@ -12,12 +12,41 @@ from ..ldap import LdapResult, LdapDatabase
 class TestCase(unittest.TestCase):
     """Test case base class"""
 
-    @staticmethod
-    def readall(ldif):
+    @classmethod
+    def resource_package(cls, name):
+        """Identify most specific package containing a named resource"""
+        for subcls in cls.__mro__:
+            if pkg_resources.resource_exists(subcls.__module__, name):
+                return subcls.__module__
+            if subcls == TestCase:
+                break
+        raise KeyError("Missing resource '%s'" % name)
+
+    @classmethod
+    def resource_string(cls, name):
+        """Get package resource content as (byte) string"""
+        return pkg_resources.resource_string(cls.resource_package(name), name)
+
+    @classmethod
+    def resource_stream(cls, name):
+        """Get package resource content as (binary) file-like object"""
+        return pkg_resources.resource_stream(cls.resource_package(name), name)
+
+    @classmethod
+    def resource_text(cls, name):
+        """Get package resource content as (text) string"""
+        return cls.resource_string(name).decode()
+
+    @classmethod
+    def resource_textio(cls, name):
+        """Get package resource content as (text) file-like object"""
+        return TextIOWrapper(cls.resource_stream(name))
+
+    @classmethod
+    def readall(cls, ldif):
         """Read all trace events from LDIF file"""
-        with pkg_resources.resource_stream(__name__, ldif) as binfh:
-            with TextIOWrapper(binfh) as fh:
-                yield from LdapResult.readall(fh)
+        with cls.resource_textio(ldif) as fh:
+            yield from LdapResult.readall(fh)
 
     @classmethod
     @contextmanager
